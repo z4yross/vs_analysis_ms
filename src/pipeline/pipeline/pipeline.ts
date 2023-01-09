b176e20120eabbda10afb458fa86432d82905e88
z4yross <z4yross@gmail.com>
2023-01-09 10:31:49 -0500

    added kubernetes client

--- src/pipeline/pipeline/pipeline.ts
+++ src/pipeline/pipeline/pipeline.ts
@@ -1,49 +1,74 @@
 import { Injectable } from '@nestjs/common';
-import { Dockerode } from 'dockerode';
+import { Client } from 'kubernetes-client';
+
+import * as PipelineManifest from './manifests/pipeline.json';
 
 // Pipeline monitor and control class for spawning and managing child processes
 // This should be a custom singleton provider 
 
 @Injectable()
 export class PipelineMonitor{
-    private readonly DOCKER_HOST: string = process.env.DOCKER_HOST || 'localhost';
-    private readonly DOCKER_PORT: string = process.env.DOCKER_PORT || '2375';
-    private readonly DOCKER_PROTOCOL: string = process.env.DOCKER_PROTOCOL || 'tcp';
+    private KubeClient: Client;
 
-    private pipelines: Map<string, any> = new Map<string, any>();
-
-    constructor(private readonly dockerode: Dockerode) {
-        if (process.env.DOCKER_SOCKET)
-            dockerode = new Dockerode({socketPath: process.env.DOCKER_SOCKET});
-        else dockerode = new Dockerode({host: this.DOCKER_HOST, port: this.DOCKER_PORT, protocol: this.DOCKER_PROTOCOL});
-    }
-
-    // pull image from dockerhub
-    async pullImage(imageName: string): Promise<void> {
-        return this.dockerode.pull(imageName);
+    constructor() {
+        this.KubeClient = new Client({ version: '1.9' });
     }
 
     // run pipiline container with given image name, and data volumes
     async runPipeline(imageName: string, provided_by: string, project_id: string): Promise<void> {
-        const volumeName: string = `/${provided_by}/${project_id}`;
 
-        const volumes = {}
+        const volumeName: string = `${provided_by}/${project_id}`;
 
-        volumes[`${volumeName}/fastq`] = '/vigilant/fastq_pass';
-        volumes[`${volumeName}/summary`] = '/vigilant/summary';
-        volumes[`${volumeName}/sample_sheet`] = '/vigilant/sample_sheet';
-        volumes[`${volumeName}/out`] = '/vigilant/out';
-        volumes[`${volumeName}/variant`] = '/vigilant/variants';
 
-        let pp = this.dockerode.run(imageName, [], process.stdout, {Volumes: volumes});
+        const deployment = PipelineManifest;
+        deployment.metadata.name = "pipeline" + '-' + provided_by.toLowerCase() + '-' + project_id.toLowerCase();
+        deployment.spec.template.spec.containers[0]['env'] = [
+            {
+                name: 'FASTQ',
+                value: `/storage/${volumeName}/fastq`
+            },
+            {
+                name: 'SUMMARY',
+                value: `/storage/${volumeName}/summary`
+            },
+            {
+                name: 'SAMPLE_SHEET',
+                value: `/storage/${volumeName}/sample_sheet`
+            },
+            {
+                name: 'OUT',
+                value: `/storage/${volumeName}/out`
+            },
+            {
+                name: 'VARIANT',
+                value: `/storage/${volumeName}/variant`
+            },
+        ];
 
-        this.pipelines.set(`${provided_by}/${project_id}`, pp);
-        return pp;
+
+        const create = await Client.apis.apps.v1.namespaces('default').deployments.post({ body: deployment });
+        return create;
     }
 
     // stop pipeline container
     async stopPipeline(provided_by: string, project_id: string): Promise<void> {
-        let pp = this.pipelines.get(`${provided_by}/${project_id}`);
-        return pp.stopPipeline();
+        await Client.apis.apps.v1.namespaces('default').deployments("pipeline" + '-' + provided_by.toLowerCase() + '-' + project_id.toLowerCase()).delete();
+    }
+
+    // get pipeline container status
+    async getPipelineStatus(provided_by: string, project_id: string): Promise<void> {
+        const deployment = await Client.apis.apps.v1.namespaces('default').deployments("pipeline" + '-' + provided_by.toLowerCase() + '-' + project_id.toLowerCase()).get();
+        return deployment;
+    }
+
+    // get pipeline container logs
+    async getPipelineLogs(provided_by: string, project_id: string): Promise<void> {
+        // get deployment 
+        const deployment = await Client.apis.apps.v1.namespaces('default').deployments("pipeline" + '-' + provided_by.toLowerCase() + '-' + project_id.toLowerCase()).get();
+        // get all pods of deployment
+        const pod = await Client.api.v1.namespaces('default').pods.get({ qs: { labelSelector: `app=${deployment.body.spec.selector.matchLabels.app}` } });
+        // get logs of pod
+        const logs = await Client.api.v1.namespaces('default').pods(pod.body.items[0].metadata.name).log.get();
+        return logs;
     }
 }
